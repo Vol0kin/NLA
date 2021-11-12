1d933963ed754e60e36a5addcaa9fb802a6031d9
Vladislav Nikolov <vladis890@gmail.com>
2021-11-12 04:09:20 +0100

    Create function that solves the KTT problem without inequality using Cholesky factorization

--- Project1/inequality_solver.py
+++ Project1/inequality_solver.py
@@ -65,8 +65,8 @@ def kkt_inequality_solver(G, C, g, d, lamb_0, s_0, x_0, max_iter=100, tol=1e-16,
         sigma = (mu_tilda / mu) ** 3
 
         # Step 4: Corrector substep
-        # The multiplication D_{\sigma} D_{\lambda} e can be substituted by the
-        # element-wise multiplication of d_s and d_\lambda
+        # The matrix multiplication D_s D_{\lambda} e can be substituted by the
+        # element-wise multiplication of d_s and d_{\lambda}
         r_s = r_s + d_s * d_lamb - sigma * mu * e
         rh_vector = np.hstack((r_L, r_C, r_s))
 
@@ -88,11 +88,9 @@ def kkt_inequality_solver(G, C, g, d, lamb_0, s_0, x_0, max_iter=100, tol=1e-16,
 
         mu = np.dot(s, lamb) / m
 
-
     total_t = time.time() - start_t
 
     return x, i, total_t, condition_numbers
-
 
 
 def kkt_inequality_ldlt_solver(G, C, g, d, lamb_0, s_0, x_0, max_iter=100, tol=1e-16, cond_num=False):
@@ -158,8 +156,8 @@ def kkt_inequality_ldlt_solver(G, C, g, d, lamb_0, s_0, x_0, max_iter=100, tol=1
         sigma = (mu_tilda / mu) ** 3
 
         # Step 4: Corrector substep
-        # The multiplication D_{\sigma} D_{\lambda} e can be substituted by the
-        # element-wise multiplication of d_s and d_\lambda
+        # The matrix multiplication D_s D_{\lambda} e can be substituted by the
+        # element-wise multiplication of d_s and d_{\lambda}
         r_3 = r_3 + d_s * d_lamb - sigma * mu * e
         rh_vector = np.hstack((r_1, r_2 - np.dot(inv_diag_lamb, r_3)))
 
@@ -185,6 +183,85 @@ def kkt_inequality_ldlt_solver(G, C, g, d, lamb_0, s_0, x_0, max_iter=100, tol=1
     return x, i, total_t, condition_numbers
 
 
+def kkt_inequality_cholesky_solver(G, C, g, d, lamb_0, s_0, x_0, max_iter=100, tol=1e-16, cond_num=False):
+    # Copy initial values (these will be modified later on)
+    x = np.copy(x_0)
+    lamb = np.copy(lamb_0)
+    s = np.copy(s_0)
+
+    # Get dimensions
+    n, m = C.shape
+
+    # Define values used in the algorithm
+    e = np.ones(m)
+    FACTOR = 0.95
+    i = 0
+    condition_numbers = []
+
+    start_t = time.time()
+
+    # Compute initial values
+    r_1, r_2, r_3, _ = solve_system(G, C, x, g, d, lamb, s)
+    mu = np.dot(s, lamb) / m
+
+    while np.linalg.norm(r_1) >= tol and np.linalg.norm(r_2) >= tol and np.linalg.norm(mu) >= tol and i < max_iter:
+        i += 1
+
+        # Compute G hat matrix and vector
+        diag_inv_s_lamb = np.diagflat(lamb / s)
+
+        G_hat = G + np.dot(C, np.dot(diag_inv_s_lamb, C.T))
+        rh_vector = r_1 - np.dot(C, (-r_3 + lamb * r_2) / s)
+
+        # Compute Cholesky factorization (no need to store previous value of G hat)
+        G_hat = spla.cholesky(G_hat)
+
+        # Compute condition number
+        if cond_num:
+            condition_numbers.append(np.linalg.cond(G_hat))
+            condition_numbers.append(np.linalg.cond(G_hat.T))
+
+        # Step 1: Solve system
+        y = spla.solve_triangular(G_hat, -rh_vector, lower=True)
+        d_x = spla.solve_triangular(G_hat.T, y)
+        d_lamb = (-r_3 + lamb * r_2) / s - np.dot(diag_inv_s_lamb, np.dot(C.T, d_x))
+        d_s = -r_2 + np.dot(C.T, d_x)
+
+        # Step 2: Step-size correction substep
+        alpha = Newton_step(lamb, d_lamb, s, d_s)
+
+        # Step 3: Compute correctors
+        mu_tilda = np.dot(s + alpha * d_s, lamb + alpha * d_lamb) / m
+        sigma = (mu_tilda / mu) ** 3
+
+        # Step 4: Corrector substep
+        # The matrix multiplication D_s D_{\lambda} e can be substituted by the
+        # element-wise multiplication of d_s and d_{\lambda}
+        r_3 = r_3 + d_s * d_lamb - sigma * mu * e
+        rh_vector = r_1 - np.dot(C, (-r_3 + lamb * r_2) / s)
+
+        y = spla.solve_triangular(G_hat, -rh_vector, lower=True)
+        d_x = spla.solve_triangular(G_hat.T, y)
+        d_lamb = (-r_3 + lamb * r_2) / s - np.dot(diag_inv_s_lamb, np.dot(C.T, d_x))
+        d_s = -r_2 + np.dot(C.T, d_x)
+
+        # Step 5: Step-size correction substep
+        alpha = Newton_step(lamb, d_lamb, s, d_s)
+
+        # Step 6: Update values and M_kkt matrix
+        x += FACTOR * alpha * d_x
+        lamb += FACTOR * alpha * d_lamb
+        s += FACTOR * alpha * d_s
+
+        r_1, r_2, r_3, _ = solve_system(G, C, x, g, d, lamb, s)
+
+        mu = np.dot(s, lamb) / m
+
+    total_t = time.time() - start_t
+
+    return x, i, total_t, condition_numbers
+
+
 n = 5
 m = 2 * n
 
@@ -196,9 +273,17 @@ x_0 = np.zeros(n)
 s_0 = np.ones(m)
 lamb_0 = np.ones(m)
 
-x_sol, i, total_t, condition_numbers = kkt_inequality_ldlt_solver(G, C, g, d, lamb_0, s_0, x_0, cond_num=False)
 print(f'Starting point: {g}')
-print(f'Solution: {x_sol}\tNum. iterations: {i}\tTime: {total_t}s')
+
+# Solve with every method and show solution and time
+x_sol, i, total_t, condition_numbers = kkt_inequality_solver(G, C, g, d, lamb_0, s_0, x_0, cond_num=False)
+print(f'Linear system solution: {x_sol}\tNum. iterations: {i}\tTime: {total_t}s')
+
+x_sol, i, total_t, condition_numbers = kkt_inequality_ldlt_solver(G, C, g, d, lamb_0, s_0, x_0, cond_num=False)
+print(f'LDL^t factorization solution: {x_sol}\tNum. iterations: {i}\tTime: {total_t}s')
+
+x_sol, i, total_t, condition_numbers = kkt_inequality_cholesky_solver(G, C, g, d, lamb_0, s_0, x_0, cond_num=False)
+print(f'Cholesky factorization solution: {x_sol}\tNum. iterations: {i}\tTime: {total_t}s')
 
 print('Condition numbers')
 print(condition_numbers)
