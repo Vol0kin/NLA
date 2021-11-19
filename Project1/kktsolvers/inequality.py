1283e2d4aef73ff5e89d42ae9f5e2e9155adfd6a
Vladislav Nikolov <vladis890@gmail.com>
2021-11-19 07:50:06 +0100

    Rename variables so that they correspond with their original names

--- Project1/kktsolvers/inequality.py
+++ Project1/kktsolvers/inequality.py
@@ -118,10 +118,10 @@ def ldlt_solver(G, C, g, d, lamb_0, s_0, x_0, max_iter=100, tol=1e-16, cond_num=
     M_kkt[:n, n:] = -C
 
     # Compute initial values
-    r_1, r_2, r_3, rh_vector = solve_system(G, C, x, g, d, lamb, s)
+    r_L, r_C, r_s, rh_vector = solve_system(G, C, x, g, d, lamb, s)
     mu = np.dot(s, lamb) / m
 
-    while np.linalg.norm(r_1) >= tol and np.linalg.norm(r_2) >= tol and np.linalg.norm(mu) >= tol and i < max_iter:
+    while np.linalg.norm(r_L) >= tol and np.linalg.norm(r_C) >= tol and np.linalg.norm(mu) >= tol and i < max_iter:
         i += 1
 
         # Compute variable block of the matrix
@@ -135,13 +135,13 @@ def ldlt_solver(G, C, g, d, lamb_0, s_0, x_0, max_iter=100, tol=1e-16, cond_num=
             condition_numbers.append(np.linalg.cond(M_kkt, 2))
 
         # Step 1: Solve system
-        rh_vector = np.hstack((r_1, r_2 - r_3 / lamb))
+        rh_vector = np.hstack((r_L, r_C - r_s / lamb))
 
         y = spla.solve_triangular(L_kkt, -rh_vector, lower=True)
         u = y / D_kkt.diagonal()
         d_z = spla.solve_triangular(L_kkt.T, u)
         d_x, d_lamb = d_z[:n], d_z[n:n+m]
-        d_s = (-r_3 - s * d_lamb) / lamb
+        d_s = (-r_s - s * d_lamb) / lamb
 
         # Step 2: Step-size correction substep
         alpha = Newton_step(lamb, d_lamb, s, d_s)
@@ -153,14 +153,14 @@ def ldlt_solver(G, C, g, d, lamb_0, s_0, x_0, max_iter=100, tol=1e-16, cond_num=
         # Step 4: Corrector substep
         # The matrix multiplication D_s D_{\lambda} e can be substituted by the
         # element-wise multiplication of d_s and d_{\lambda}
-        r_3 = r_3 + d_s * d_lamb - sigma * mu * e
-        rh_vector = np.hstack((r_1, r_2 - r_3 / lamb))
+        r_s = r_s + d_s * d_lamb - sigma * mu * e
+        rh_vector = np.hstack((r_L, r_C - r_s / lamb))
 
         y = spla.solve_triangular(L_kkt, -rh_vector, lower=True)
         u = y / D_kkt.diagonal()
         d_z = spla.solve_triangular(L_kkt.T, u)
         d_x, d_lamb = d_z[:n], d_z[n:n+m]
-        d_s = (-r_3 - s * d_lamb) / lamb
+        d_s = (-r_s - s * d_lamb) / lamb
 
         # Step 5: Step-size correction substep
         alpha = Newton_step(lamb, d_lamb, s, d_s)
@@ -170,7 +170,7 @@ def ldlt_solver(G, C, g, d, lamb_0, s_0, x_0, max_iter=100, tol=1e-16, cond_num=
         lamb += FACTOR * alpha * d_lamb
         s += FACTOR * alpha * d_s
 
-        r_1, r_2, r_3, rh_vector = solve_system(G, C, x, g, d, lamb, s)
+        r_L, r_C, r_s, rh_vector = solve_system(G, C, x, g, d, lamb, s)
 
         mu = np.dot(s, lamb) / m
 
@@ -197,17 +197,17 @@ def cholesky_solver(G, C, g, d, lamb_0, s_0, x_0, max_iter=100, tol=1e-16, cond_
     start_t = time.time()
 
     # Compute initial values
-    r_1, r_2, r_3, _ = solve_system(G, C, x, g, d, lamb, s)
+    r_L, r_C, r_s, _ = solve_system(G, C, x, g, d, lamb, s)
     mu = np.dot(s, lamb) / m
 
-    while np.linalg.norm(r_1) >= tol and np.linalg.norm(r_2) >= tol and np.linalg.norm(mu) >= tol and i < max_iter:
+    while np.linalg.norm(r_L) >= tol and np.linalg.norm(r_C) >= tol and np.linalg.norm(mu) >= tol and i < max_iter:
         i += 1
 
         # Compute G hat matrix and vector
         diag_inv_s_lamb = np.diagflat(lamb / s)
 
         G_hat = G + np.dot(C, np.dot(diag_inv_s_lamb, C.T))
-        rh_vector = r_1 - np.dot(C, (-r_3 + lamb * r_2) / s)
+        rh_vector = r_L - np.dot(C, (-r_s + lamb * r_C) / s)
 
         # Compute Cholesky factorization (no need to store previous value of G hat)
         G_hat = spla.cholesky(G_hat)
@@ -219,8 +219,8 @@ def cholesky_solver(G, C, g, d, lamb_0, s_0, x_0, max_iter=100, tol=1e-16, cond_
         # Step 1: Solve system
         y = spla.solve_triangular(G_hat, -rh_vector, lower=True)
         d_x = spla.solve_triangular(G_hat.T, y)
-        d_lamb = (-r_3 + lamb * r_2) / s - np.dot(diag_inv_s_lamb, np.dot(C.T, d_x))
-        d_s = -r_2 + np.dot(C.T, d_x)
+        d_lamb = (-r_s + lamb * r_C) / s - np.dot(diag_inv_s_lamb, np.dot(C.T, d_x))
+        d_s = -r_C + np.dot(C.T, d_x)
 
         # Step 2: Step-size correction substep
         alpha = Newton_step(lamb, d_lamb, s, d_s)
@@ -232,13 +232,13 @@ def cholesky_solver(G, C, g, d, lamb_0, s_0, x_0, max_iter=100, tol=1e-16, cond_
         # Step 4: Corrector substep
         # The matrix multiplication D_s D_{\lambda} e can be substituted by the
         # element-wise multiplication of d_s and d_{\lambda}
-        r_3 = r_3 + d_s * d_lamb - sigma * mu * e
-        rh_vector = r_1 - np.dot(C, (-r_3 + lamb * r_2) / s)
+        r_s = r_s + d_s * d_lamb - sigma * mu * e
+        rh_vector = r_L - np.dot(C, (-r_s + lamb * r_C) / s)
 
         y = spla.solve_triangular(G_hat, -rh_vector, lower=True)
         d_x = spla.solve_triangular(G_hat.T, y)
-        d_lamb = (-r_3 + lamb * r_2) / s - np.dot(diag_inv_s_lamb, np.dot(C.T, d_x))
-        d_s = -r_2 + np.dot(C.T, d_x)
+        d_lamb = (-r_s + lamb * r_C) / s - np.dot(diag_inv_s_lamb, np.dot(C.T, d_x))
+        d_s = -r_C + np.dot(C.T, d_x)
 
         # Step 5: Step-size correction substep
         alpha = Newton_step(lamb, d_lamb, s, d_s)
@@ -248,7 +248,7 @@ def cholesky_solver(G, C, g, d, lamb_0, s_0, x_0, max_iter=100, tol=1e-16, cond_
         lamb += FACTOR * alpha * d_lamb
         s += FACTOR * alpha * d_s
 
-        r_1, r_2, r_3, _ = solve_system(G, C, x, g, d, lamb, s)
+        r_L, r_C, r_s, _ = solve_system(G, C, x, g, d, lamb, s)
 
         mu = np.dot(s, lamb) / m
 
