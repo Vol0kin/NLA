1d5ba76920849a4b295bc62cf2b13f5fb665ff8a
Vladislav Nikolov <vladis890@gmail.com>
2021-12-07 03:30:51 +0100

    Add compression for color images

--- Project2/image_compression.py
+++ Project2/image_compression.py
@@ -2,19 +2,46 @@ import numpy as np
 import imageio
 import argparse
 
-def compress_image(image_path, rank):
-    image = imageio.imread(image_path)
-
-    if image.ndim != 2:
-        image = image[:, :, 0]
-
-    # Use SVD to compute a low rank approximation of the original image
+def bw_compression(image, rank):
     u, s, v = np.linalg.svd(image, full_matrices=False)
     compressed_image = np.dot(u[:, :rank], np.dot(np.diagflat(s[:rank]), v[:rank, :]))
 
     # Compute error of the low rank approximation using Frobenius norm
     image_norm = np.linalg.norm(image)
     error = np.linalg.norm(image - compressed_image) / image_norm
+
+    return compressed_image, error
+
+
+def color_compression(image, rank):
+    compressed_image = []
+    errors = []
+
+    for d in range(3):
+        image_single_channel = image[:, :, d]
+
+        # Compress the channel
+        u, s, v = np.linalg.svd(image_single_channel, full_matrices=False)
+        compressed_channel = np.dot(u[:, :rank], np.dot(np.diagflat(s[:rank]), v[:rank, :]))
+        compressed_image.append(compressed_channel)
+
+        # Compute error of the low rank approximation of the channel using the Frobenius norm
+        image_norm = np.linalg.norm(image_single_channel)
+        errors.append(np.linalg.norm(image_single_channel - compressed_image) / image_norm)
+    
+    compressed_image = np.dstack(compressed_image)
+    error = np.mean(errors)
+
+    return compressed_image, error
+    
+
+def compress_image(image_path, rank):
+    image = imageio.imread(image_path)
+
+    if image.ndim == 2:
+        compressed_image, error = bw_compression(image, rank)
+    else:
+        compressed_image, error = color_compression(image, rank)
 
     # Save image
     imageio.imwrite(f'k{rank}_{round(error, 4)}_{image_path}', compressed_image)
